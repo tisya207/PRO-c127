c6d8d60b3b0f635f709feedb662125d61d04f494
tisya207 <65344664+tisya207@users.noreply.github.com>
2022-03-13 10:08:25 +0530

    Add files via upload

--- code.py
+++ code.py
@@ -1,9 +1,11 @@
 from bs4 import BeautifulSoup
+from selenium import webdriver
+import requests
 import time
 import csv
 
 start_url= "https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars"
-browser= webdriver.Chrome("chromedriver.exe")
+browser= webdriver.Chrome("./chromedriver")
 
 browser.get(start_url)
 time.sleep(10)
